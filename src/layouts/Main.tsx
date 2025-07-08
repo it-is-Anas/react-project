43772a15483159b3c6976b318f4f89d12a17fecf
prontoTheLegend <anasprog8@gmail.com>
2025-07-08 17:57:23 +0300

    React ts with tailwind

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -2,14 +2,6 @@ import { Outlet , Link } from "react-router-dom";
 
 export default function MainLayout() {
     return (
-        <div>
-            <div className="">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                </ul>
-            </div>
-            <Outlet />
-        </div>
+        <div className="bg-[red]">sss</div>
     )
 }
