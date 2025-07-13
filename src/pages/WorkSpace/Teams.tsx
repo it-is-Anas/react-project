ffde8afc21a6987fe5d03bf5b7b2bca175c07d95
prontoTheLegend <anasprog8@gmail.com>
2025-07-13 13:22:59 +0300

    Team card finished

--- src/pages/WorkSpace/Teams.tsx
+++ src/pages/WorkSpace/Teams.tsx
@@ -1,5 +1,15 @@
+import TeamCard from "../../components/Card/TeamCard";
+
 export default function Teams (){
+    const arr = [];
+    for(let i = 0; i < 1 ; ++i)arr.push(<TeamCard key={i} />);
+
     return (
+    <>
         <h1>Teams</h1>
+        <div className="w-[100%] max-h-[calc(100%-2.5em)] justify-items-center  grid grid-cols-[repeat(auto-fill,minmax(16em,1fr))] gap-4 p-4 overflow-y-scroll"  >
+            { arr } 
+        </div>
+    </>
     );
 }
