e511f00760797687bd374c923c69114408b55b06
prontoTheLegend <anasprog8@gmail.com>
2025-07-20 21:59:55 +0300

    use same function (time) in project

--- src/pages/WorkSpace/Project.tsx
+++ src/pages/WorkSpace/Project.tsx
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 import axios from "axios";
 import { setTasks } from "../../Store/Slices/Task";
 import { useParams } from "react-router-dom";
+import { timeAgo } from "../../Helpers/Time";
 const BACK_URL = import.meta.env.VITE_API_URL;
 
 
@@ -39,24 +40,6 @@ export default function Project(){
     },[]);
 
 
-    function timeAgo(dateString: string): string {
-        const date = new Date(dateString);
-        const now = new Date();
-        const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000); 
-        if (diffInSeconds < 60) return 'just now';
-        const minutes = Math.floor(diffInSeconds / 60);
-        
-        if (minutes < 60) return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
-
-        const hours = Math.floor(minutes / 60);
-        if (hours < 24) return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
-
-        const days = Math.floor(hours / 24);
-        if (days < 7) return `${days} day${days !== 1 ? 's' : ''} ago`;
-
-        const weeks = Math.floor(days / 7);
-        return `${weeks} week${weeks !== 1 ? 's' : ''} ago`;
-    }
 
     const project: (Project| null) = useSelector((state: RootState)=> state.project.project);
 
